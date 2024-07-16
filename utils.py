7848b15c1c45cccd72756b4d608d469e64ae8a1e
Fedor Gorlenko <fedorgorlenko@gmail.com>
2024-07-17 01:00:57 +0300

    names upper

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ from string import ascii_uppercase
 def to_name(name:str) -> str:
     full_name = []
     for i in name:
-        full_name.append(i[0].upper() + i[1:].lower())
+        full_name.append(i.upper())
     if len(full_name) < 3:
         full_name.append('')
     return full_name
+
 
 def coords_to_xl(x: int, y: int) -> str:
     return ascii_uppercase[x] + str(y + 1)
